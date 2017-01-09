f9dbfcf353098dc5b1bc45dc716d322cd3248393
Sinutok <mglocal@PGEG.metzler.com>
2017-01-09 15:22:33 +0100

    Initial
    Alles noch leer

---
+++ pyiMap.py
@@ -0,0 +1,25 @@
+#
+# Diverses IMAP-Zeugs in Python
+# 
+# Der erste Versuch
+#
+params = {}    # Mein Repository - Unelegant aber praktisch
+
+
+# IMPORTS
+from time import localtime,strftime
+from sys import argv
+import optparse
+
+
+
+# ################################
+# 
+# MAIN
+#
+if __name__ = '__main__':
+   """ Wir sind nicht inportierbar, as ususal """
+   
+   
+   
+
