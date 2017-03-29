c9cd93b51ef9533804c5694cb93b08c452a5f3de
Sinutok <1307github@gmx.de>
2017-03-29 18:01:51 +0200

    Ein Commit
    damit WingIDE es vielleicht dann sieht

--- pyiMap.py
+++ pyiMap.py
@@ -2,7 +2,12 @@
 # Diverses IMAP-Zeugs in Python
 # 
 # Der erste Versuch
+# Das Ganze wird in 3.6+ geschrieben, aber wird sehr 2.xig aussehen, weil dies das erste 3er Projekt ist.
+# Code-Stil-Aenderungen sind wahrscheinlich, schluderei auch.
 #
+# 2017-03-29 - Philospohieren
+#
+
 params = {}    # Mein Repository - Unelegant aber praktisch
 
 
