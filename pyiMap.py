d9e16b558dbb0ba530d70c627a910357415ef6b9
Sinutok <1307github@gmx.de>
2017-04-11 18:26:08 +0200

    Erste Config, beginn logging

--- pyiMap.py
+++ pyiMap.py
@@ -13,17 +13,48 @@ params = {}    # Mein Repository - Unelegant aber praktisch
 
 # IMPORTS
 from time import localtime,strftime
-from sys import argv
-import optparse
+from sys import argv, exit
+import configparser              # Damit mit einer INI gearbeitet werden kann
+import logging                   # LOG-Mechanismus
 
 
+params['DEBUGLEVEL']:int   = 0
+params['LogFile']:str      = ""
 
 # ################################
 # 
 # MAIN
 #
-if __name__ = '__main__':
+if __name__ == '__main__':
    """ Wir sind nicht inportierbar, as ususal """
+   
+   pyiMapConfig = configparser.ConfigParser()   # Configparser initialisieren
+   
+   pyiMapConfig.read("./pyiMap.ini")            # Ini-File einlesen
+   
+   # Checken ob es ein Defaul gibt
+   if 'Default' not in pyiMapConfig:
+      print("[Default]-Sektion in ini fehlt, oder INI-File unlesbar!")
+      exit(-1)
+      
+   # DEBUGLEVEL finden
+   try:
+      params['DEBUGLEVEL'] = pyiMapConfig['Default']['Debuglevel']
+   except:
+      print("Default -> Loglevel nicht erkennbar")
+      exit(-1)
+      
+   try:
+      params['LogFile'] = pyiMapConfig['Default']['LogFile']
+   except:
+      print("Default -> Logfile nicht gesetzt")
+      exit(-1)
+   
+   
+
+   
+   
+   
    
    
    
